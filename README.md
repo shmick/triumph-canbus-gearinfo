c53be04b08112da1f6ce9fd920e69021411661f1
Steve M <shmick@users.noreply.github.com>
2023-05-18 22:08:28 -0400

    merge dev to main (#2)

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# triumph-canbus-gearinfo
+Using [CircuitPython](https://circuitpython.org/) on a [Wemos S2 Mini](https://circuitpython.org/board/lolin_s2_mini/) with an inexpensive MCP2515 CAN Bus module (eBay, Amazon, etc ) to read the gear selection on a 2012 Triumph Thunderbird
+
+The CAN Bus message ID is `0x540`
+
+The selected gear is located in the first byte of the message data, using the binary value of bits 6 to 4 
+
+|Gear| Hex Range | Binary Range        |Bits 6-4|
+|:-:|:----------:|:-------------------:|:------:|
+| N |0x00 - 0x0F | 00000000 - 00001111 |  000  |
+| 1 |0x10 - 0x1F | 00010000 - 00011111 |  001  |
+| 2 |0x20 - 0x2F | 00100000 - 00101111 |  010  |
+| 3 |0x30 - 0x3F | 00110000 - 00111111 |  011  |
+| 4 |0x40 - 0x4F | 01000000 - 01001111 |  100  |
+| 5 |0x50 - 0x5F | 01010000 - 01011111 |  101  |
+| 6 |0x60 - 0x6F | 01100000 - 01101111 |  110  |
+
+An example of a message for 5th gear
+
+`ID: 0x540 Data: ('0x5C', '0x2C', '0x01', '0x00', '0x00', '0x00', '0x00')`
+
+The first byte shows `0x5C` which is in the range of `0x50` to `0x5F`
+
+Credit to DickC on the [triumph675.net](https://www.triumph675.net/threads/ecu-to-dash-can-bus-message-ids.242889/) forum
---
+++ main.py
@@ -0,0 +1,92 @@
+import board
+import busio
+import digitalio
+from adafruit_mcp2515.canio import Message, Match
+from adafruit_mcp2515 import MCP2515 as CAN
+
+
+cs = digitalio.DigitalInOut(board.IO5)
+cs.switch_to_output()
+# D5/IO7 = SCK, D7/IO11 = MOSI, D6/IO9 = MISO
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+mcp = CAN(spi, cs, baudrate=1000000)
+
+match_id = 0x540
+match_ids = [
+    Match(address=match_id, mask=0x7FF),
+    Match(address=match_id, mask=0x7FF),
+]
+
+# IO37 is connected to the gate pin of a BS170 MOSFET
+# This will control a 12v LED to indicate that the motorcycle
+# is in 6th gear
+led = digitalio.DigitalInOut(board.IO37)
+led.direction = digitalio.Direction.OUTPUT
+led.value = False
+
+debug_ignore_ids = ()
+debug = 0
+
+"""
+**********************************************************
+There should not be any reason to modify things below here
+**********************************************************
+"""
+
+
+def report_gear(msg_id, msg_data):
+    gear_map = {
+        0: "N",
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+    }
+
+    # from https://www.triumph675.net/threads/ecu-to-dash-can-bus-message-ids.242889/
+    # byte 0 - bits 6...4 - Gear Position - 0 = N, 1-6 = gears 1-6
+
+    # turn the hex string into an integer
+    byte_0 = int(msg_data[0], 16)
+
+    # 0x70 is a mask that selects only the bits 6 to 4
+    # the >> operator shifts them to the right by 4 positions to align them with the least significant bit
+    bits_6_to_4 = (byte_0 & 0x70) >> 4
+
+    gear = gear_map.get(bits_6_to_4, "")
+    if gear:
+        if msg_id == match_id and len(msg_data) == 7:
+            print(f"*********\nGear: {gear}\n*********\n")
+
+    if gear == "6":
+        led.value = True
+    else:
+        led.value = False
+
+
+def print_message(msg):
+    if isinstance(msg, Message) and msg.data:
+        msg_data = tuple("0x{:02X}".format(x) for x in msg.data)
+        print(f"ID: {hex(msg.id)} Data: {msg_data}")
+        report_gear(msg.id, msg_data)
+
+
+def main_loop():
+    with mcp.listen(matches=match_ids, timeout=0.1) as listener:
+        while True:
+            msg = listener.receive()
+            if msg and not debug:
+                print_message(msg)
+            elif debug and msg.id not in ignore_ids:
+                print_message(msg)
+
+
+if __name__ == "__main__":
+    print(f"baudrate: {mcp.baudrate}\nDebug: {debug}")
+    ignore_ids = ()
+    if debug:
+        ignore_ids = debug_ignore_ids
+        match_ids = []
+    main_loop()
